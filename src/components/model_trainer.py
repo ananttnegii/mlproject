7ca24193ea8873bf7a569bdc6d5f923860986506
Anant <69311730+ananttnegii@users.noreply.github.com>
2026-03-05 21:08:04 +0530

    Hyper Parameter Training

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -39,12 +39,42 @@ class ModelTrainer:
             models= {
                 "Random Forest":RandomForestRegressor(),
                 "Decision Tree":DecisionTreeRegressor(),
-                "Dradient Boosting":GradientBoostingRegressor(),
+                "Gradient Boosting":GradientBoostingRegressor(),
                 "Linear Regressor":LinearRegression(),
                 "K-Neighbours Classifier":KNeighborsRegressor(),
-                "XGB Clssifier":XGBRegressor(),
+                "XGB Classifier":XGBRegressor(),
                 "Catboosting Classifier":CatBoostRegressor(verbose=False),
                 "Adaboost Classifier":AdaBoostRegressor()
+            }
+            params= {
+                "Decision Tree":{
+                    'criterion':['squared_error','friedman_mse','absolute_error','poisson']
+                },
+                "Random Forest":{
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "Linear Regressor":{},
+                "K-Neighbours Classifier":{
+                    'n_neighbors':[5,7,9,11]
+                },
+                "XGB Classifier":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "Catboosting Classifier":{
+                    'depth':[6,8,10],
+                    'learning_rate':[.01,.05,.1],
+                },
+                "Adaboost Classifier":{
+                    'learning_rate':[.1,.01,.5,.001],
+                    'n_estimators':[8,16,32,64,128,256]
+                }
+
             }
 
             model_report:dict=evaluate_models(
@@ -52,7 +82,8 @@ class ModelTrainer:
                 Y_train= Y_train, 
                 X_test= X_test,
                 Y_test= Y_test,
-                models= models)
+                models= models,
+                params=params)
             
             ## To get best model score from dict 
 
--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import pandas as pd
 from src.exception import CustomException
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -19,13 +20,20 @@ def save_object(file_path, obj):
         from src.exception import CustomException
         raise CustomException(e,sys)
     
-def evaluate_models(X_train,Y_train,X_test,Y_test,models):
+def evaluate_models(X_train,Y_train,X_test,Y_test,models,params):
     try:
         report= {}
         for i in range(len(list(models))):
             model= list(models.values())[i]
+            para=params[list(models.keys())[i]]
 
-            model.fit(X_train,Y_train) # train model
+            gs=GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,Y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,Y_train)
+
+            # model.fit(X_train,Y_train) # train model
 
             y_train_pred= model.predict(X_train)
             y_test_pred= model.predict(X_test)
